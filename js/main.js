b91cd24d1f81e01c75b87a5d9668fd3e7619ac81
Abdoul <abdoul.m2408@gmail.com>
2020-06-05 17:28:40 +0200

    event on key press/up (JS)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('#card-crypt').on('click', ()=>{
+$('#card-crypt').on('click', () => {
     $('.card').css('opacity', '0');
     $('.card').css('z-index', '0');
     $('.card').css('cursor', 'default')
@@ -18,6 +18,41 @@ $('#card-crypt').on('click', function (event) {
     event.stopPropagation();
 });
 
-$('#card-crypt').on('keyup', ()=>{
+$('#card-crypt').on('keyup', () => {
     $('.print-crypt input').val($('#card-crypt').val());
 });
+
+function onlyNumber(evt) {
+    if (evt.which > 31 && (evt.which < 48 || evt.which > 57)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+$('#card-number').on('keyup', (evt) => {
+    if ($('#card-number').val().length == 0) {
+        for (let i = 0; i < 16; i++) {
+            $('#' + i).text('#');
+        }
+    } else {      
+        for (let i = 0; i < $('#card-number').val().length; i++) {
+            if (evt.key === 'Backspace') {
+                console.log(i);
+                $('#' + i).text('#');
+            } else {
+                let replace = $('#card-number').val().charAt(i);
+                $('#' + i).text(replace);
+            }
+        }
+    }
+});
+
+
+function onlyChar(evt) {
+    if (evt.which > 31 && (evt.which < 48 || evt.which > 57)) {
+        return true;
+    } else {
+        return false;
+    }
+}
