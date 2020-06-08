9e139f896c2ee1100e10a6413949a39f2e3481f4
Abdoul <abdoul.m2408@gmail.com>
2020-06-08 17:02:30 +0200

    ajout animation JQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var today = new Date();
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+
+$('span[data-name= month]').html(mm);
+$('span[data-name= sep]').html(' / ');
+$('span[data-name= year]').html(yyyy);
+
 $('#card-crypt').on('click', () => {
     $('.card').css('opacity', '0');
     $('.card').css('z-index', '0');
@@ -35,19 +43,24 @@ $('#card-number').on('keyup', (evt) => {
         for (let i = 0; i < 16; i++) {
             $('#' + i).text('#');
         }
-    } else {      
+    } else {
         for (let i = 0; i < $('#card-number').val().length; i++) {
             if (evt.key === 'Backspace') {
-                console.log(i);
-                $('#' + i).text('#');
+                let pos = $('#card-number').val().length;
+                // console.log($('#card-number').val().length);
+                $('#' + pos).fadeOut(400, function () {
+                    $('#' + pos).text('#').fadeIn(400);
+                });
             } else {
+                let pos2 = $('#card-number').val().length - 1;
                 let replace = $('#card-number').val().charAt(i);
-                $('#' + i).text(replace);
+                $('#' + pos2).fadeOut(400, function () {
+                    $('#' + pos2).text(replace).fadeIn(400);
+                });
             }
         }
     }
 });
-
 
 function onlyChar(evt) {
     if (evt.which > 31 && (evt.which < 48 || evt.which > 57)) {
@@ -56,3 +69,40 @@ function onlyChar(evt) {
         return false;
     }
 }
+
+$('#card-name').on('keyup', (evt) => {
+    if ($('#card-name').val().length == 0) {
+        $('.print-name p').html('Will Smith');
+    } else {
+        $('.fill-name').html('<p></p>');
+        for (let i = 0; i < $('#card-name').val().length; i++) {
+            if (evt.key != 'Backspace') {
+                let replace = $('#card-name').val().charAt(i);
+                $('.fill-name').fadeOut(400, function () {
+                    $('.fill-name').append('<p data-id=' + i + '>' + replace + '</p>').fadeIn(400);
+                });
+            } else if (evt.key === 'Backspace') {
+                $('.fill-name').fadeOut(400, function () {
+                    $('.fill-name').html($('#card-name').val()).fadeIn(400);
+                });
+            }
+        }
+    }
+});
+
+$('#card-date').on('change', () => {
+    $('span[data-name= month]').fadeOut(400, function () {
+        $('span[data-name= month]').html($('#card-date').val()).fadeIn(400);
+    });
+})
+
+$('#card-year').on('change', () => {
+    $('span[data-name= year]').fadeOut(400, function () {
+        $('span[data-name= year]').html($('#card-year').val()).fadeIn(400);
+    });
+})
+
+
+
+
+
